29411bb660b537f32c83294d7e7f62cd0b1b8102
badrm22 <badrm0279@gmail.com>
2022-07-01 10:50:14 -0700

    fixed whitespace

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,16 @@
 int main(void)
 {
 	int num1;
-	
+
 	int num2;
-	
+
 	for (num1 = 0; num1 < 9; num1++)
 	{
 		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
 			putchar('0' + ((num1 % 10)));
 			putchar('0' + ((num2 % 10)));
-			
+
 			if (num1 == 8 && num2 == 0)
 			{
 				continue;
@@ -30,6 +30,6 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
