cfb6b0454e604c1435e757cb844cbae57216a4e6
badrm22 <badrm0279@gmail.com>
2022-07-01 10:19:46 -0700

    base16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+  * main - print all the numbers of base 16 in lowercase
+  *
+  *Return: 0
+  */
+
+int main(void)
+{
+	int n;
+
+	char c;
+
+	for (; n < 10; i++)
+	{
+		putchar('0' + ((n % 10)));
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		printchar(c);
+	}
+
+	printchar('\n');
+
+	return (0);
+
+}
+
