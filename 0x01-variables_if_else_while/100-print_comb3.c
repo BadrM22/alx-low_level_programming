77c751d218d49ef51e80a978426bc68e99737c93
badrm22 <badrm0279@gmail.com>
2022-07-01 10:46:45 -0700

    print combo 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+  * main - print all possible different combinations
+  * of tow digits separated by comma then space
+  *
+  *return: 0
+  */
+
+int main(void)
+{
+	int num1;
+	
+	int num2;
+	
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 10; num2++)
+		{
+			putchar('0' + ((num1 % 10)));
+			putchar('0' + ((num2 % 10)));
+			
+			if (num1 == 8 && num2 == 0)
+			{
+				continue;
+			}
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	
+	return (0);
+}
