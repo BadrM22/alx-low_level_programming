0b8520aa0a1c6e9dff2b6b70a06e28364b233d4e
badrm22 <badrm0279@gmail.com>
2022-07-01 08:20:31 -0700

    fixed typo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else if (last_digit > 5)
 	{
-		printf("Last digit if %d is %d and is greater than 5\n", n,\
+		printf("Last digit of %d is %d and is greater than 5\n", n,\
 		last_digit);
 	}	
 	else
