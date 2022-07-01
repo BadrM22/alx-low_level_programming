fc715e4bc7adaae1ef8e3af96bce09b347761c9c
badrm22 <badrm0279@gmail.com>
2022-07-01 08:58:38 -0700

    print all signle digit nums

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - print all single digit numbers
+  * of base 10 starting from 0
+  * Return : Always 0
+  */
+
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+	{
+		putchar('0' + ((i % 10)));
+	}
+
+	putchar('\n');
+
+	return (0);
+}
