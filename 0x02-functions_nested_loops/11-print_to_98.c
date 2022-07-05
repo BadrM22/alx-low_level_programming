016903156cb7f2b28b8d6248ba23383286eb6238
badrm22 <badrm0279@gmail.com>
2022-07-05 12:21:05 -0700

    fixed stdout issue

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,18 +15,21 @@ void print_to_98(int n)
 	{
 		for (; n < 98; n++)
 		{
-			printf("%d\n", n);
+			_putchar("0", n);
+			_putchar('\n');
 		}
 	}
 	else if (n > 98)
 	{
 		for (; n > 98; n--)
 		{
-			printf("%d\n", n);
+			_putchar("0", n);
+			_putchar('\n');
 		}
 	}
 	if (n == 98)
 	{
-		printf("%d\n", n);
+		_putchar("0", n);
+		_putchar('\n');
 	}
 }
