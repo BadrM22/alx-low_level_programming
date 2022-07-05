0acc14a301b942182bb719bd17614c32588457b4
badrm22 <badrm0279@gmail.com>
2022-07-05 09:01:24 -0700

    update prototype

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,3 @@
 #include "_putchar.c"
 
 void print_alphabet(void);
-
-/**
- * print_alphabet: prints alphabet characters in lowercase
- * Return: void
- */
-
-void print_alphabet(void)
-{
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
-}
-
