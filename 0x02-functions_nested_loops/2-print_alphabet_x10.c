d080adcba430e47982bcb8ca1ab967576e18ecb8
badrm22 <badrm0279@gmail.com>
2022-07-05 09:35:49 -0700

    print lowercase alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - prints alphabet in lowercase 10 times
+  * seperated by newline.
+  * Return: void
+  */
+
+void print_alphabet_x10(void)
+{
+	short i = 0;
+
+	char c;
+
+	while (i < 10)
+	{
+		c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+
+			c++;
+		}
+		_putchar('\n');
+	}
+}
+
