6410f239f0de92780159ca097bb74c0687228ecb
badrm22 <badrm0279@gmail.com>
2022-07-05 10:04:23 -0700

    check if char is alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+/**
+ * _isalpha - Function to check for alphabet
+ * @c: input char to check
+ *
+ * Return: 1 if alphabet else return 0
+ */
+
+int _isalpha(int c)
+{
+	if ( (c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
