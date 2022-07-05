ee6c4882d86c24b08c4ddc063d072ecc387f23e9
badrm22 <badrm0279@gmail.com>
2022-07-05 10:47:11 -0700

    find abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ * _abs - Function to get the absolute value of an integer
+ * @i: Integer number to get its absolute value
+ *
+ * Return: int
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+		return (i * (-1));
+	return (i);
+}
