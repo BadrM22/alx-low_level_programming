4c8532c13d54b896fdf8d8827549d9f5c06804f7
badrm22 <badrm0279@gmail.com>
2022-07-05 13:38:37 -0700

    get first 50 fib

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int fib(int n);
+
+/**
+ * main - Program to print the first 50 Fibonacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (int x = 0; x < 51; x++)
+	{
+		printf("%d, ", fib(x));
+	}
+	return (0);
+}
+
+/**
+ * fib - Function to return a fibonacci number
+ * @n: input to get fib number
+ *
+ * Return: int
+ */
+
+int fib(int n)
+{
+	if (n < 2)
+	{
+		return (1);
+	}
+	return (fib(n - 1) + fib(n - 2));
+}
