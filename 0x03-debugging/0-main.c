a0eb989d250ec203d67ca609ebfced2be8788a3e
badrm22 <badrm0279@gmail.com>
2022-07-06 07:20:46 -0700

    initial commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Program to test that the function
+ * positive_or_negative gives the correct output
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
