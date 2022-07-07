6ec05b80899d1690998b895936433d50ddb56d0c
badrm22 <badrm0279@gmail.com>
2022-07-07 03:30:48 -0700

    fixed extra space in code

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,9 +17,9 @@ void print_triangle(int size)
 	}
 	for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j <= size; j++)
+		for (j = 1; j <= size; j++)
 		{
-			if ( j <= (size - i))
+			if (j <= (size - i))
 			{
 				_putchar(' ');
 			}
