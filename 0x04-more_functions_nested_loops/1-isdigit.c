aaef95876a95b5361f6b4a57d3bb871cf8bc2710
badrm22 <badrm0279@gmail.com>
2022-07-06 23:17:17 -0700

    check if a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+/**
+ * _isdigit - Function to check if acharacter is a digit
+ * @c: input character to check
+ *
+ * Return: return 1 @c is a digit else 0
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
