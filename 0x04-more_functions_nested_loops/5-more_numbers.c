16f33ce3824c1c3e30dea3f26a6911ec78df2f0d
badrm22 <badrm0279@gmail.com>
2022-07-07 00:15:27 -0700

    print from 0 to 14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+/**
+ * more_numbers - Function to print numbers from 0 to 14
+ * 10 times.
+ *Return: void
+ */
+
+void more_numbers(void)
+{
+	int count, num;
+
+	count = 0;
+	num = 0;
+
+	while (count < 10)
+	{
+		while (num < 15)
+		{
+			if (num > 9)
+			{
+				_putchar('0' + (num / 10));
+			}
+			_putchar('0' + (num % 10));
+			num++;
+		}
+		_putchar('\n');
+		num = 0;
+		count++;
+	}
+}
