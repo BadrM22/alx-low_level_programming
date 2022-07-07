c53e44425e5e03c10d729bfea34ebcea6298c4e3
badrm22 <badrm0279@gmail.com>
2022-07-06 23:41:31 -0700

    print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+/**
+ * print_numbers - Function to print numbers from 0 to 9.
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
