144ba3291d177bbe9169ec908e22293a15d03932
badrm22 <badrm0279@gmail.com>
2022-07-11 05:45:22 -0700

    print array elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_array - Function to print elements of an array
+ * @a: array
+ * @n: size of array
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+}
