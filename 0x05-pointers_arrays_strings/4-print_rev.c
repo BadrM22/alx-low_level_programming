6698094da7fcc898126b6c5497fac59d709380a7
badrm22 <badrm0279@gmail.com>
2022-07-11 02:42:49 -0700

    reverse a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - takes a pointer to a string ad reverse string to stdout
+ * @str - input pointer to a string
+ *
+ * Return void
+ */
+
+void print_rev(char *s)
+{
+	while (*s++ != '\0')
+	{
+		_putchar(*(s - 2));
+	}
+	_putchar('\n');
+}
