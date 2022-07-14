e7b5294562fc15c98ceaf2a013d688f1430b6bde
badrm22 <badrm0279@gmail.com>
2022-07-14 09:08:37 -0700

    encode string in leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * leet - Function that encodes a string to leet
+ * @s: string to be encoded
+ *
+ * Return: char pointer
+ */
+
+char *leet(char *s)
+{
+	int i;
+
+	char *m = {'a', 'e', 'o', 't', 'l'};
+
+	char *M = {'A', 'E', 'O', 'T', 'L'};
+
+	int code[] = {4, 3, 0, 7, 1};
+
+	while (*s != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*s == *(m + i) || *s == *(M + i))
+			{
+				*s = code[i];
+			}
+		}
+		s++;
+	}
+	return (s);
+}
