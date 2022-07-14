1c993ea87dd4b490a699afaf525523490c2cebe2
badrm22 <badrm0279@gmail.com>
2022-07-14 03:18:41 -0700

    concatenates n byte of a string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - Function that concatenates n bytes of a string
+ * to another string
+ * @dest: string to append to
+ * @src: string to be appended
+ * @n: number to bytes of @src to be appended to @dest
+ *
+ * Return: pointer
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = j = 0;
+
+	while (*(dest + i))
+	{
+		i++;
+	}
+	for (j = 0; j < n; j++)
+	{
+		dest[i + j] = *(src + j);
+	}
+	dest[j + i] = '\0';
+	return (dest);
+}
