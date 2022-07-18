881f4c0cf12f41f819d2337475a632815494f68a
badrm22 <badrm0279@gmail.com>
2022-07-17 22:16:45 -0700

    locate a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+/**
+ * _strchr - Function that locates a character in a string
+ * @s: input string to search in
+ * @c: character to search for in @s
+ *
+ * Return: char Pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if ((int) *s == (int) c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (NULL);
+}
