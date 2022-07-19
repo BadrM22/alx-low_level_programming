578cbf53d140f92081f23092098ecfac78f18825
badrm22 <badrm0279@gmail.com>
2022-07-19 11:39:02 -0700

    natural square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+/**
+ * sqrt - functino to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to a
+ *
+ * Return: 1 on success else -1
+ */
+
+int sqrt2(int a, int b)
+{
+	if (b * b == a)
+	{
+		return (b);
+	}
+	else if (b * b > a)
+	{
+		return (-1);
+	}
+	return (sqrt2(a, b + 1));
+}
+
+/**
+ * _sqrt_recursion - function to get natural square root of n
+ * @n : Integer number
+ *
+ * Return: 1 on success else -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1));
+}
