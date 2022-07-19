ac4d52438f89e0449015b60aac8cbbda9713b69e
badrm22 <badrm0279@gmail.com>
2022-07-19 11:15:37 -0700

    raise to power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _pow_recursion - Function that returns a power of a number
+ * @x: int number
+ * @y: power value
+ *
+ * Return: @x raised to power of @y if success else -1 if y is lower
+ * than 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
