49775e72acda81ac7f6633076f8f5d2b9b204860
badrm22 <badrm0279@gmail.com>
2022-07-22 06:22:49 -0700

    program to print the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Program that it's name
+ * @argc: count of command line arguments
+ * @argv: command line arguments
+ * Return: 0 if success else 1
+ */
+
+int main(int argc, char **argv)
+{
+	_putchar(argv[0]);
+	_putchar('\n');
+
+	return (0);
+}
