67d4eaa980f24e625f77607e71a4b486161d1e41
badrm22 <badrm0279@gmail.com>
2022-07-22 07:25:53 -0700

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program to print the result of multiplying
+ * two numbers
+ * @argc: count of commandline argumnets
+ * @argv: command line argumnets
+ *
+ * Return: 0 on success else 1
+ */
+
+int main(int argc, char **argv)
+{
+	int n1, n2;
+
+	if (argc > 1)
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+
+		printf("%d\n", n1 * n2);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
