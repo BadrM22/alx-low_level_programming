ddf1a7feb5251d2c6d52c130e4d811a347d8b18f
badrm22 <badrm0279@gmail.com>
2022-07-22 06:57:22 -0700

    print command line arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print all command line argumnets
+ * @argc: count of command line argumnets
+ * @argv: command line argumnets
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argc;
+	
+	while (*argv)
+	{
+		printf("%s\n", *argv);
+		argv++;
+	}
+	return (0);
+}
