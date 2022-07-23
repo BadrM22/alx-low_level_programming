db3563d008f26deb774d97be5f87a6be5cb41bba
badrm22 <badrm0279@gmail.com>
2022-07-23 01:00:09 -0700

    fixed code

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,17 +14,15 @@ int main(int argc, char **argv)
 {
 	int n1, n2;
 
-	if (argc > 1)
-	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
-
-		printf("%d\n", n1 * n2);
-		return (0);
-	}
-	else
+	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+		return (0);
+
 }
