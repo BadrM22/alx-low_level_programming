bf020599f6e38d4116ce0d764fb1f9018dd6e487
badrm22 <badrm0279@gmail.com>
2022-07-23 18:25:09 -0700

    fixed code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,9 +9,18 @@
 
 char *create_array(unsigned int size, char c)
 {
+	int i;
+
+	char *ptr;
+	
 	if (size <= 0)
 	{
 		return (NULL);
 	}
-	return ((char *) malloc(size * sizeof(c)));
+	ptr = (char *) malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
 }
