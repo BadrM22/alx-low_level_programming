6bb81adae072dc982a00779dbed64de41b9b9b7b
badrm22 <badrm0279@gmail.com>
2022-07-23 23:34:24 -0700

    concat string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * str_concat - Function that allocate new memory for two
+ * concatenated strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer on success else null
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *s = NULL;
+	int i, j;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	s = (char *) malloc((strlen(s1) + strlen(s2)) * 8);
+	if (!s)
+	{
+		return (NULL);
+	}
+	i = 0;
+
+	while (*(s1 + i))
+	{
+		s[i] = *(s1 + i);
+		i++;
+	}
+	j = 0;
+
+	while (*(s2 + j))
+	{
+		s[i + j] = *(s2 + j);
+		j++;
+	}
+	s[i + j] = '\0';
+	return (s);
+}
