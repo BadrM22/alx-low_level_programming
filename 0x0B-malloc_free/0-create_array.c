4c598918c5dc0c0806d0b4960828bf0f1e26b7c4
badrm22 <badrm0279@gmail.com>
2022-07-23 18:32:03 -0700

    handeled malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,11 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	ptr = (char *) malloc(size * sizeof(char));
-	for (i = 0; i < (size * 8); i++)
+	if (ptr == null)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
 	}
