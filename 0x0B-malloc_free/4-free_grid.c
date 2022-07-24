3e18845f9ab04573346287dce60ce966a9e65b75
badrm22 <badrm0279@gmail.com>
2022-07-24 01:43:25 -0700

    free 2-D array memory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#incldue "main.h"
+
+/**
+ * free_grid - function to free memory location of a 2-D array
+ * @grid: 2-D array to be freed
+ * @height: length of @grid
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
