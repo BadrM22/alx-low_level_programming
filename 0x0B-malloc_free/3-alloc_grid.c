07c52648fd88e8c3844f0d741f871c3dd7006d71
badrm22 <badrm0279@gmail.com>
2022-07-24 01:43:06 -0700

    fixed code

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	int **matrix = (int **) malloc(height * sizeof(int *));
+	matrix = (int **) malloc(height * sizeof(int *));
 
 	if (*matrix == NULL)
 	{
