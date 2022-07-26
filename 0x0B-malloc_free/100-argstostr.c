d2bf3bb5e78f9f49016c9f35c340b1aefa9a4c67
badrm22 <badrm0279@gmail.com>
2022-07-26 07:10:45 -0700

    concatenate command line argumnets

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * argstostr - Function that concatenates all command line arguments
+ * separated by a new line.
+ * @ac: command line arguments count
+ * @av: command line arguments pointer
+ *
+ * Return: pointer to a string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str = NULL;
+	int i, j, idx = 0, len = 0;
+
+	if (ac <= 1 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 1; i < ac; i++)
+	{
+		len += av[i];
+	}
+	len += ac;
+
+	str = (char *) malloc(sizeof(char) * len);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 1; i < ac; i++)
+	{
+		for (j = 0; j < strlen(av[i]); j++)
+		{
+			str[idx] = av[i][j];
+			idx++;
+		}
+		if (str[idx] == '\0')
+		{
+			str[idx++] = '\n';
+		}
+	}
+	return (str);
+}
