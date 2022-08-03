0b0923e40e2284bf781ca7cc6529cad7d571c600
badrm22 <badrm0279@gmail.com>
2022-08-03 10:08:35 -0700

    execute a functino on an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+/**
+ * array_iterator - Functino that executes another function
+ * given as a parameter on each element of an array
+ * @array: input array
+ * @size: length of array
+ * @action: pointer to a function to be executed
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
